ccc68d24cffdfdd615280c88800e220fa7081c47
Vinisha02 <vinisha.choudhary02@gmail.com>
2024-02-20 15:29:05 +0530

    project 1 React static pages

---
+++ src/pages/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Navbar from "./api/components/Navbar"
+import Main from "./api/components/Main"
+
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
